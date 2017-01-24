cc653fbaf653ec8f4bc03b22259545e696f947a5
rhodesvr <HeavenlyBerserker>
2017-01-24 15:59:13 -0600

    Commit 01/24/2017 Fixed grabbing and clonning implementations to code, controller scripts modified.

--- graphics_assignment/Assets/_scripts/PickupParent.cs
+++ graphics_assignment/Assets/_scripts/PickupParent.cs
@@ -17,6 +17,7 @@ public class PickupParent : MonoBehaviour
     private bool holdt = false;
     private bool itemh = false;
     public GameObject sc;
+    private Transform tra2;
 
     Collider globalcol = new Collider();
     int yRot = 0;
@@ -26,16 +27,26 @@ public class PickupParent : MonoBehaviour
         trackedobj = GetComponent<SteamVR_TrackedObject>();
     }
 
+    bool abovePlat() {
+        if (globalcol)
+        {
+            Transform loc = gameObject.GetComponent<Transform>();
+            Vector3 c = loc.position;
+
+            if (c.x < -9.9 && c.x > -19.7 && c.z < -73.3 && c.z > -83.1 && c.y > 21.2)
+            {
+                return true;
+
+            }
+        }
+        return false;
+    }
+
     void Update()
     {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedobj.index);
 
         
-
-    }
-    
-    void FixedUpdate()
-    {
-        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedobj.index);
 
         if(globalcol) globalcol.gameObject.transform.rotation = Quaternion.Euler(-90, yRot, 0);
 
@@ -47,34 +58,26 @@ public class PickupParent : MonoBehaviour
             Transform loc = gameObject.GetComponent<Transform>();
             Vector3 c = loc.position;
 
-            if (c.x < -9.9 && c.x > -19.7 && c.z < -73.3 && c.z > -83.1 && c.y > 21.2) {
+            if (abovePlat()) {
                 double xx = c.x + 9.9; double zz = c.z + 73.3;
                 xx = Math.Round(xx / .30625); zz = Math.Round(zz / .30625);
                 xx *= .30625; zz *= .30625;
                 xx -= 9.9; zz -= 73.3;
                 c.x = (float)xx; c.z = (float)zz;
 
+                Vector3 p = c;
+                //Modify the height of the shadow cast
+                p.y = 21.2f;
+                //
+                tra2.position = p;
+
                 if (globalcol) globalcol.gameObject.transform.position = c;
             }
         }
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
         {
-            //Debug.Log("TouchPad Up");
-            yRot += 45;
-
-            /*
-            Collider coll = globalcol.gameObject.GetComponent<Collider>();
-            Vector3 c = coll.bounds.center;
-            //c = c - new Vector3(c.x %.1f, c.y%.1f, c.z%.1f);
-            Vector3 ma = coll.bounds.max;
-            Vector3 mi = coll.bounds.min;
-            
-
-            shadow = Instantiate(globalcol.gameObject, c, Quaternion.Euler(-90, yRot, 0)) as GameObject;
-
-            Transform tra2 = shadow.GetComponent<Transform>();
-            tra2.localScale = new Vector3(1,1,1);*/
-
+            Debug.Log("TouchPad Up");
+            yRot += 90;
         }
 
         
@@ -82,7 +85,7 @@ public class PickupParent : MonoBehaviour
 
     void OnTriggerStay(Collider col)
     {
-        
+
         SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedobj.index);
         //Debug.Log("You have collided with " + col.name + " and activated OnTriggerStay");
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger) && itemh == false)
@@ -92,6 +95,23 @@ public class PickupParent : MonoBehaviour
             col.gameObject.transform.SetParent(gameObject.transform);
             globalcol = col;
             if (globalcol) Debug.Log("Grab: " + globalcol.gameObject.name);
+
+            //Creating shadow---------
+            Collider coll = globalcol.gameObject.GetComponent<Collider>();
+            Vector3 c = coll.bounds.center;
+            Vector3 ma = coll.bounds.max;
+            Vector3 mi = coll.bounds.min;
+
+
+            shadow = Instantiate(globalcol.gameObject, c, Quaternion.Euler(-90, yRot, 0)) as GameObject;
+
+            tra2 = shadow.GetComponent<Transform>();
+            Vector3 p = tra2.position;
+            p.y = 22;
+            tra2.position = p;
+            tra2.localScale = new Vector3(1, 1, 1);
+            //----------------------
+
             itemh = true;
         }
         else if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
@@ -106,23 +126,23 @@ public class PickupParent : MonoBehaviour
             //tossObject(col.attachedRigidbody);
         }
     }
-    
+
     //void tossObject (Rigidbody rb)
 
     //{
-        //SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedobj.index);
-        ///Transform origin = trackedobj.origin ? trackedobj.origin : trackedobj.transform.parent;
-        //if (origin != null)
-        //{
-           // rb.velocity = origin.TransformVector(device.velocity * speed);
-            //rb.angularVelocity = origin.TransformVector(device.angularVelocity * speed);
+    //SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedobj.index);
+    ///Transform origin = trackedobj.origin ? trackedobj.origin : trackedobj.transform.parent;
+    //if (origin != null)
+    //{
+    // rb.velocity = origin.TransformVector(device.velocity * speed);
+    //rb.angularVelocity = origin.TransformVector(device.angularVelocity * speed);
 
-        //}
-        //else
-        //{
-            //rb.velocity = device.velocity*speed;
-            //rb.angularVelocity = device.angularVelocity*speed;
-        //}
     //}
-    
+    //else
+    //{
+    //rb.velocity = device.velocity*speed;
+    //rb.angularVelocity = device.angularVelocity*speed;
+    //}
+    //}
+
 }
